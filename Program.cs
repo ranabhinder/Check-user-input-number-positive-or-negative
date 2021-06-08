398695fa568092241b016724432c5784ac0ccc86
ranabhinder1321 <ranabhinder@yahoo.co.nz>
2021-06-08 20:43:58 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Check_user_input_number_positive_or_negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            // Asks the user to enter a number
+
+            Console.WriteLine("Please enter first number");
+            //Below line converts the input from a string to an integer
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0)
+            {
+                Console.WriteLine("Enter number is positive ");
+            }
+
+            else if (num < 0)
+            {
+                Console.WriteLine("Enter number is negative ");
+            }
+
+
+                
+        }
+    }
+}
